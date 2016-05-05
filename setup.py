2c828931d6396ddd6374f70dd7e7ffdacc90d805
sirrice <ewu@cs.columbia.edu>
2016-05-05 11:37:58 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='latexsnapshots',
-    version='0.0.7',
+    version='0.0.8',
     description='Create snapshots of your latex papers + UI',
     url='https://github.com/sirrice/latexsnapshots',
     author='Eugene wu',
